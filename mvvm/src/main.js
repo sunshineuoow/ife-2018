0db8032e4f9f6f9359499fd5b61a60a743823388
sunshineuoow <xiongjunwen0315@163.com>
2018-05-23 16:01:23 +0800

    增加数据操作

--- mvvm/src/main.js
+++ mvvm/src/main.js
@@ -1,7 +1,9 @@
 import san from 'san'
 import { router } from 'san-router' 
 import App from './App.san'
+import Demo01 from './demo01/Demo01.san'
 
 router.add({rule: '/', Component: App, target: "#app"})
+router.add({rule: '/demo01', Component: Demo01, target: '#app'})
 
 router.start()
